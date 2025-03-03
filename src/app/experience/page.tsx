0c3228cadffe950cd543e183bfaadb2ee8f0bb29
Cody West <cww2697@gmail.com>
2025-03-02 22:45:07 -0600

    Add experience page

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -1,18 +1,35 @@
+import data from "../values/experience.json";
+import ExperienceListItem from "@/app/components/ExperienceItem/ExperienceListItem";
+
 export default function Page() {
-    /*interface Experience {
+    interface Experience {
         id: number;
         position: string;
-        company: string;
-        desc: string;
+        company?: string;
+        city: string;
+        state: string;
+        descLine1: string;
+        descLine2?: string
         startDate: string;
         endDate: string;
-    }*/
+    }
 
     return (
         <>
             <div className="contentContainer">
-                <h1>Experience Page</h1>
-                <p>This is the experience page</p>
+                {Object.values(data).reverse().map((experience: Experience) => (
+                    <ExperienceListItem
+                        key={experience.id}
+                        position={experience.position}
+                        company={experience.company}
+                        city={experience.city}
+                        state={experience.state}
+                        descLine1={experience.descLine1}
+                        descLine2={experience.descLine2}
+                        startDate={experience.startDate}
+                        endDate={experience.endDate}
+                    />
+                ))}
             </div>
         </>
     );
